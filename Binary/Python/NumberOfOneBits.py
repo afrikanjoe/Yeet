4b8672d3f255ab1abfbb47c373e28fb577d55f57
Patrick Musau <pmusau13ster@gmail.com>
2020-12-10 10:10:04 -0600

    Number of bits

--- Binary/Python/NumberOfOneBits.py
+++ Binary/Python/NumberOfOneBits.py
@@ -1,3 +1,18 @@
+"""
+n & n-1 will remove the right most 1 from n.
+(n-1) flips right-most 1 and everything right of that
+tip: you can use n & n-1 == 0 to check whether a given number n is a power of 2 because 
+all powers of 2 only have a single 1. i.e 
+n = 32 = 100000
+ &
+n-1=31 = 011111
+----------------
+n&(n-1)= 000000
+
+"""
+
+
+
 class Solution:
     def hammingWeight(self,n):
         n = "{0:b}".format(n)
@@ -7,8 +22,16 @@ class Solution:
                 count+=1
         return count
 
+    def hammingWeightMagic(self,n):
+        count = 0 
+        while (n!=0):
+            n = n & (n -1)
+            count+=1
+        return count
+
 
 if __name__=="__main__":
     inp = 5
     sol = Solution()
     print("Brute Force:",sol.hammingWeight(inp))
+    print("Magic:",sol.hammingWeightMagic(inp))
