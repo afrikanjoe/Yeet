a57278e946b1dc8b70f4f32754bf696d863cca16
Patrick Musau <pmusau13ster@gmail.com>
2020-12-18 20:37:19 -0600

    Kumbe this was harder than we thought

--- Recursion/Python/power.py
+++ Recursion/Python/power.py
@@ -9,6 +9,18 @@ class Solution:
         else:
             return x * Solution.power(x,n-1)
 
+    @staticmethod
+    def power1(x, n):
+        if(n==0):
+            return 1 
+        if(n>0):
+            if(n%2==0):
+                return Solution.power1(x*x,n/2)
+            else:
+                return x * Solution.power1(x*x,(n - 1) / 2)
+        else:
+            return 1.0 / Solution.power1(x,-n)
+
 if __name__ == "__main__":
     print(Solution.power(4,-3))
     print(Solution.power(4,3))
