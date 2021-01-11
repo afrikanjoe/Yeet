5a0d9151186f5342f46a59066db1d0cb23e268f2
Patrick Musau <pmusau13ster@gmail.com>
2021-01-11 17:30:50 -0600

    sorted

--- Two Pointers/Python/RemoveDuplicatesFromSorted.py
+++ Two Pointers/Python/RemoveDuplicatesFromSorted.py
@@ -2,9 +2,13 @@
 Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 """
 
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
 
 class Solution:
-    def deleteDuplicates(self, head: ListNode) -> ListNode:
+    def deleteDuplicates(self, head):
         if(head == None):
             return head
         vals = []
