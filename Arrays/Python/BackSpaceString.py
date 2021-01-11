ab8fd8318f3bc7e31b6b4357ae3e5c091c032c3e
Patrick Musau <pmusau13ster@gmail.com>
2021-01-11 14:10:34 -0600

    works now

--- Arrays/Python/BackSpaceString.py
+++ Arrays/Python/BackSpaceString.py
@@ -10,12 +10,23 @@ class Solution:
     def dobackspace(self,s):
         while ('#' in s):
             ind = s.index('#')
-            if(ind-1>=0):
-                rem = s[ind-1]
-                s= s.replace(rem,'').replace('#','',1)
+            if(ind ==0): 
+                s = s[0:ind]+s[ind+1:]
+            else:
+                s = s[0:ind-1]+s[ind+1:]
+            return s 
+            
+
         return s
 if __name__ == '__main__':
     S = "ab##" 
     T = "c#d#"
+    S1 = "#a#c"
+    T1 = "a##c"
+    S2= "oi###mupo##rszty#s#xu###bxx##dqc#gdjz"
+    S3="oi###mu#ueo##pk#o##rsztu#y#s#xu###bxx##dqc#gz#djz"
+    ss = s2 = Solution().backspaceCompare(S2,S3)
     s = Solution().backspaceCompare(S,T)
     print(s)
+    s2 = Solution().backspaceCompare(S1,T1)
+    print(s2)
