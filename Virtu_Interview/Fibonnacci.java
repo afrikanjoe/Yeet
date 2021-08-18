66bfc47959a70963283c08678db67aff74044607
afrikanjoe <joseph.mugisha@richmond.edu>
2021-08-18 06:18:33 -0500

    edits

--- Virtu_Interview/Fibonnacci.java
+++ Virtu_Interview/Fibonnacci.java
@@ -5,24 +5,19 @@ public class Fibonnacci {
         if (fib == 0)
             return 0;
 
-        int first = 0;
         int second = 1;
         int third = 1;
 
         for (int i = 3; i <= fib; i++) {
-            first = second;
             second = third;
             third = second + third;
-
         }
-
         return third;
-
     }
 
     public static void main(String[] args) {
 
-        System.out.println(fib1(2));
+        System.out.println(fib1(5));
 
     }
 
