0a4ce031336f09010e01f478d15824c2bf5f1995
pmusau17 <pmusau13ster@gmail.com>
2021-10-15 07:06:21 -0500

    We out here

---
+++ Facebook/Python/BalancedParantheses.py
@@ -0,0 +1,72 @@
+import math
+# Add any extra import statements you may need here
+
+
+# Add any helper functions you may need here
+
+
+def isBalanced(s):
+  # Write your code here
+  bracket_stack = []
+  for i in s: 
+    if(i=='('):
+      bracket_stack.append(")")
+    elif(i =="["):
+      bracket_stack.append("]")
+    elif(i =="{"):
+      bracket_stack.append("}")
+    else:
+      item = bracket_stack.pop()
+      if(item!=i):
+        return False
+  return True
+
+
+
+
+
+
+
+
+
+
+
+
+# These are the tests we use to determine if the solution is correct.
+# You can add your own at the bottom.
+
+def printString(string):
+  print('[\"', string, '\"]', sep='', end='')
+
+test_case_number = 1
+
+def check(expected, output):
+  global test_case_number
+  result = False
+  if expected == output:
+    result = True
+  rightTick = '\u2713'
+  wrongTick = '\u2717'
+  if result:
+    print(rightTick, 'Test #', test_case_number, sep='')
+  else:
+    print(wrongTick, 'Test #', test_case_number, ': Expected ', sep='', end='')
+    printString(expected)
+    print(' Your output: ', end='')
+    printString(output)
+    print()
+  test_case_number += 1
+
+if __name__ == "__main__":
+  s1 = "{[(])}"
+  expected_1 = False
+  output_1 = isBalanced(s1)
+  check(expected_1, output_1)
+
+  s2 = "{{[[(())]]}}"
+  expected_2 = True
+  output_2 = isBalanced(s2)
+  check(expected_2, output_2)
+
+  # Add your own test cases here
+  
