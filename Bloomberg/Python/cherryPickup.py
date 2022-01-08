6bcff6ceb02414cf482b752fb79e713955a45b43
pmusau17 <pmusau13ster@gmail.com>
2022-01-08 16:41:42 -0600

    DP again

--- Bloomberg/Python/cherryPickup.py
+++ Bloomberg/Python/cherryPickup.py
@@ -1,4 +1,20 @@
+"""
+You are given a rows x cols matrix grid representing a field of cherries where grid[i][j] represents the number of cherries that you can collect from the (i, j) cell.
 
+You have two robots that can collect cherries for you:
+
+Robot #1 is located at the top-left corner (0, 0), and
+Robot #2 is located at the top-right corner (0, cols - 1).
+Return the maximum number of cherries collection using both robots by following the rules below:
+
+From a cell (i, j), robots can move to cell (i + 1, j - 1), (i + 1, j), or (i + 1, j + 1).
+When any robot passes through a cell, It picks up all cherries, and the cell becomes an empty cell.
+When both robots stay in the same cell, only one takes the cherries.
+Both robots cannot move outside of the grid at any moment.
+Both robots should reach the bottom row in grid.
+
+
+"""
 class Solution:
     def cherryPickup(self, grid):
         maxa = -2**32
@@ -45,3 +61,37 @@ class Solution:
             if(not added):
                 maxa = max(maxa,paths[1][-1][2]+paths[0][-1][2])
         return maxa
+
+from functools import lru_cache
+
+
+class OptimalSolution:
+    def cherryPickup(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+        
+        @lru_cache(None)
+        def helper(row,col1,col2):
+            if(col1<0 or col2<0 or col1>=n or col2>=n):
+                return - 2**32
+            
+            result = 0 
+            result+=grid[row][col1]
+            if(col1!=col2):
+                result+=grid[row][col2]
+                
+            if(row<m-1):
+                curr_row = row+1
+                sub_cases = [helper(curr_row,new_col1,new_col2) 
+                             for new_col1 in [col1,col1+1,col1-1]
+                             for new_col2 in [col2,col2+1,col2-1]
+                            ]
+                result += max(sub_cases)
+                
+            return result
+        
+        return helper(0,0,n-1)
+
+if __name__ == "__main__":
+    nums = [[8,8,10,9,1,7],[8,8,1,8,4,7],[8,6,10,3,7,7],[3,0,9,3,2,7],[6,8,9,4,2,5],[1,1,5,8,8,1],[5,6,5,2,9,9],[4,4,6,2,5,4],[4,4,5,3,1,6],[9,2,2,1,9,3]]
+    print(OptimalSolution().cherryPickup(nums))
